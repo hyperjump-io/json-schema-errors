07bda938c0fda5743d9d4a056d6580bfcad02205
Sam <samailahmad61@gmail.com>
2026-01-28 12:18:04 +0530

    combined multipleOf messages

--- src/error-handlers/multipleOf.js
+++ src/error-handlers/multipleOf.js
@@ -6,10 +6,38 @@ import * as Instance from "@hyperjump/json-schema/instance/experimental";
  * @import { ErrorHandler, ErrorObject } from "../index.d.ts"
  */
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+const gcd = (a, b) => {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return Math.abs(a);
+};
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+const lcm = (a, b) => {
+  return Math.abs(a * b) / gcd(a, b);
+};
 /** @type ErrorHandler */
 const multipleOfErrorHandler = async (normalizedErrors, instance, localization) => {
   /** @type ErrorObject[] */
   const errors = [];
+
+  /**
+   * @type number[] */
+  const multipleOfValues = [];
+  /** @type string[] */
+  const schemaLocations = [];
 
   for (const schemaLocation in normalizedErrors["https://json-schema.org/keyword/multipleOf"]) {
     if (normalizedErrors["https://json-schema.org/keyword/multipleOf"][schemaLocation]) {
@@ -19,10 +47,16 @@ const multipleOfErrorHandler = async (normalizedErrors, instance, localization) 
     const keyword = await getSchema(schemaLocation);
     const multipleOf = /** @type number */ (Schema.value(keyword));
 
+    multipleOfValues.push(multipleOf);
+    schemaLocations.push(schemaLocation);
+  }
+
+  if (multipleOfValues.length > 0) {
+    const combineMultipleOf = multipleOfValues.reduce((acc, val) => lcm(acc, val), multipleOfValues[0]);
     errors.push({
-      message: localization.getMultipleOfErrorMessage(multipleOf),
+      message: localization.getMultipleOfErrorMessage(combineMultipleOf),
       instanceLocation: Instance.uri(instance),
-      schemaLocations: [schemaLocation]
+      schemaLocations: schemaLocations
     });
   }
 
