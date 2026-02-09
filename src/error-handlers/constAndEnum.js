42e7cca113f6ada595ce93a3cc2f2d76533dbe4b
Jason Desrosiers <jdesrosi@gmail.com>
2026-02-09 10:34:38 -0800

    Simplify const/enum

--- src/error-handlers/constAndEnum.js
+++ src/error-handlers/constAndEnum.js
@@ -4,7 +4,7 @@ import * as Instance from "@hyperjump/json-schema/instance/experimental";
 import jsonStringify from "json-stringify-deterministic";
 
 /**
- * @import { ErrorHandler, ErrorObject, Json } from "../index.d.ts"
+ * @import { ErrorHandler, Json } from "../index.d.ts"
  */
 
 /**
@@ -16,83 +16,63 @@ import jsonStringify from "json-stringify-deterministic";
 
 /** @type {ErrorHandler} */
 const constAndEnumErrorHandler = async (normalizedErrors, instance, localization) => {
-  /** @type {ErrorObject[]} */
-  const errors = [];
+  /** @type Set<string> | undefined */
+  let allowedJson;
 
-  /** @type {Constraint[]} */
-  const constraints = [];
-  let hasFailure = false;
+  /** @type string[]> */
+  const constSchemaLocations = [];
+
+  /** @type string[]> */
+  const enumSchemaLocations = [];
+
+  /** @type string[]> */
+  const allSchemaLocations = [];
 
   for (const schemaLocation in normalizedErrors["https://json-schema.org/keyword/const"]) {
-    const passed = normalizedErrors["https://json-schema.org/keyword/const"][schemaLocation] === true;
-    if (!passed) {
-      hasFailure = true;
+    if (!normalizedErrors["https://json-schema.org/keyword/const"][schemaLocation]) {
+      constSchemaLocations.push(schemaLocation);
     }
+    allSchemaLocations.push(schemaLocation);
+
     const keyword = await getSchema(schemaLocation);
-    constraints.push({
-      allowedValues: [Schema.value(keyword)],
-      schemaLocation
-    });
+    const keywordJson = new Set([jsonStringify(/** @type Json */ (Schema.value(keyword)))]);
+
+    allowedJson = allowedJson?.intersection(keywordJson) ?? keywordJson;
   }
 
   for (const schemaLocation in normalizedErrors["https://json-schema.org/keyword/enum"]) {
-    const passed = normalizedErrors["https://json-schema.org/keyword/enum"][schemaLocation] === true;
-    if (!passed) {
-      hasFailure = true;
+    if (!normalizedErrors["https://json-schema.org/keyword/enum"][schemaLocation]) {
+      enumSchemaLocations.push(schemaLocation);
     }
+    allSchemaLocations.push(schemaLocation);
+
     const keyword = await getSchema(schemaLocation);
-    constraints.push({
-      allowedValues: Schema.value(keyword),
-      schemaLocation
-    });
+    const keywordJson = new Set(/** @type Json[] */ (Schema.value(keyword)).map((value) => jsonStringify(value)));
+
+    allowedJson = allowedJson?.intersection(keywordJson) ?? keywordJson;
   }
 
-  if (!hasFailure || constraints.length === 0) {
-    return errors;
+  if (constSchemaLocations.length === 0 && enumSchemaLocations.length === 0) {
+    return [];
   }
 
-  let intersectionKeys = new Set(constraints[0].allowedValues.map(toKey));
-  for (let i = 1; i < constraints.length; i++) {
-    intersectionKeys = intersectionKeys.intersection(new Set(constraints[i].allowedValues.map(toKey)));
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  const intersection = /** @type {Json[]} */ ([...intersectionKeys].map((k) => JSON.parse(k)));
-
-  const instanceLocation = Instance.uri(instance);
-  const intersectionKeysArray = [...intersectionKeys];
-  const exactMatch = constraints.find((c) => {
-    if (c.allowedValues.length !== intersection.length) return false;
-    const constraintKeys = new Set(c.allowedValues.map(toKey));
-    return intersectionKeysArray.every((k) => constraintKeys.has(k));
-  });
-
-  if (intersection.length === 0) {
-    errors.push({
+  if (allowedJson?.size === 0) {
+    return [{
       message: localization.getBooleanSchemaErrorMessage(),
-      instanceLocation,
-      schemaLocations: constraints.map((c) => c.schemaLocation)
-    });
-  } else if (intersection.length === 1) {
-    errors.push({
-      message: localization.getConstErrorMessage(intersection[0]),
-      instanceLocation,
-      schemaLocations: exactMatch ? [exactMatch.schemaLocation] : constraints.map((c) => c.schemaLocation)
-    });
+      instanceLocation: Instance.uri(instance),
+      schemaLocations: allSchemaLocations
+    }];
   } else {
-    errors.push({
-      message: localization.getEnumErrorMessage(intersection),
-      instanceLocation,
-      schemaLocations: exactMatch ? [exactMatch.schemaLocation] : constraints.map((c) => c.schemaLocation)
-    });
-  }
-  return errors;
-};
+    /** @type Json[] */
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    const allowedValues = [...allowedJson ?? []].map((json) => JSON.parse(json));
 
-/**
- * @param {Json} val
- * @returns {string}
- */
-const toKey = (val) => jsonStringify(val);
+    return [{
+      message: localization.getEnumErrorMessage(allowedValues),
+      instanceLocation: Instance.uri(instance),
+      schemaLocations: constSchemaLocations.length ? constSchemaLocations : enumSchemaLocations
+    }];
+  }
+};
 
 export default constAndEnumErrorHandler;
